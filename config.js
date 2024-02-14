948089b9deac7a33752623d3c77fd320e1cbdaec
Leni <21904426@etu.unicaen.fr>
2024-02-14 13:57:00 +0100

    fix config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const config = {};
 config.postgres = {
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
-    database: process.env.POSTGRES_DATABASE,
+    database: process.env.POSTGRES_DB,
     user: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
     ssl: process.env.POSTGRES_SSL,
